e5f7520680c99f989488ab80b4eacf3f3cc850cd
chenyuan <csjhecy@sina.com>
2017-03-30 19:53:04 +0800

    修改引导页面bug,增加转场动画

--- MobilePlayer/app/src/main/java/www/csj/mobileplayer/activity/MainActivity.java
+++ MobilePlayer/app/src/main/java/www/csj/mobileplayer/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package www.csj.mobileplayer.activity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.transition.Fade;
 
 import www.csj.mobileplayer.R;
 
@@ -9,6 +11,11 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // 允许使用transitions
+            getWindow().setEnterTransition(new Fade());
+            getWindow().setReenterTransition(new Fade());
+        }
         setContentView(R.layout.activity_main);
     }
 }
--- MobilePlayer/app/src/main/java/www/csj/mobileplayer/activity/SplashActivity.java
+++ MobilePlayer/app/src/main/java/www/csj/mobileplayer/activity/SplashActivity.java
@@ -1,8 +1,10 @@
 package www.csj.mobileplayer.activity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.Toast;
 
 import www.csj.mobileplayer.R;
 
@@ -10,6 +12,9 @@ import www.csj.mobileplayer.R;
  * Created by Administrator on 2017/3/29 0029.
  */
 public class SplashActivity extends BaseActivity {
+
+    private Handler mHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +27,8 @@ public class SplashActivity extends BaseActivity {
      * 初始化一些数据，延时三秒进入apk
      */
     private void initData() {
-        new Handler().postDelayed(new Runnable() {
+        mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 goToMainActivity();
@@ -34,6 +40,19 @@ public class SplashActivity extends BaseActivity {
      * 进入主页面
      */
     private void goToMainActivity() {
-        Toast.makeText(this,"进入主页面",Toast.LENGTH_SHORT).show();
+       // Toast.makeText(this,"进入主页面",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, MainActivity.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+        } else {
+            startActivity(intent);
+        }
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHandler.removeCallbacksAndMessages(null);
     }
 }
